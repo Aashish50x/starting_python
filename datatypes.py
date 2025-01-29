70678a819234440c9f2f80bfcef9451c2e8e6e76
aashish50x <aashish50x@gmail.com>
2025-01-29 20:03:42 +0530

    All types of data types

---
+++ datatypes.py
@@ -0,0 +1,29 @@
+# using all data types
+
+# integer data tyope
+a=5
+print(a)
+
+# string data type
+b="aashish"
+print(b)
+
+# floating point integer/number
+c=12.567
+print(c)
+
+# bollean variable choice True/False 
+d=False
+print(d)
+
+# none type of variable
+e = None
+print(e)
+
+
+# vaiable name rules
+
+# A variable name can contain alphabets, digits, and underscores.
+# A variable name can only start with an alphabet and underscores.   
+# A variable name can't start with a digit.
+# No white space is allowed to be used inside a variable name.
