c2d22005075a15392366777e14cfb9dd4b412032
aashish50x <aashish50x@gmail.com>
2025-01-29 23:52:54 +0530

    finding data type by 'type()' function

---
+++ variable_type.py
@@ -0,0 +1,28 @@
+# finding data type by 'type()' function
+
+# integer data tyope
+a=5
+w=type(a)
+print("a = 5 is a ",w)
+
+# string data type
+b="aashish"
+s= type(b)
+print("b = 'aashish' is a ",s)
+
+# floating point integer/number
+c=12.567
+t = type(c)
+print("c = 12.567 is a ",t)
+
+# bollean variable choice True/False 
+d=False
+x = type(d)
+print("d = False is a ",x)
+
+# none type of variable
+e = None
+y=type(e)
+print("e = None is a ",y)
+
+# for geting data type use " type(varaiable_name)" function 
