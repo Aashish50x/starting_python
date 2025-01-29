78c13edeaf1b7a1e02b851e2e946e8cbdbea99f4
aashish50x <aashish50x@gmail.com>
2025-01-30 00:05:26 +0530

    converting data types

---
+++ datatype_conversion.py
@@ -0,0 +1,25 @@
+# integer data tyope
+a=5
+w=type(a)
+print("a is ", a)
+print("a = 5 is a ",w)
+
+# now we convert this int in float because it is valid and cam be convertable
+a1 = float(a)
+print("a = 5 is a ",type(a1))
+print("now a is ", a1)
+
+# now covert srting data input of number to float 
+
+b="5.32"
+print(b)
+print(type(b))
+# now we convert this str in float because it is valid and cam be convertable
+b1= float(b)
+print(b1)
+print(type(b1))
+
+# wrong conversion cannot possible like
+#         string of alphabets in int or float
+#         convert float to int 
+# these are impossible to convert
