f358cef7aebd85869692e0f749b78edf3369c954
aashish50x <aashish50x@gmail.com>
2025-01-30 01:28:19 +0530

    program to compare both greater or smaller

---
+++ basic_calculations2.py
@@ -0,0 +1,6 @@
+# program to show wheather the given number is greater or smaller than second one
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+print("a is greater than b :", a>b)
+print("b is greater than a :", a<b)
