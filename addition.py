f27f028c2b98352170c7407ab628f22bb9dac189
aashish50x <aashish50x@gmail.com>
2025-01-30 00:25:11 +0530

    addition of two variables

---
+++ addition.py
@@ -0,0 +1,13 @@
+# adding two number
+# a= int(input("ENTER 1st NO. :"))
+# b= int(input("ENTER 2nd NO. :"))
+# # printing sum 
+# print("sum of a and b is: ",a+b)
+ 
+
+# now adding two input 
+# always rembet that input data is always considered as string if no data type is specified
+# string is always wriiten without specifing any datatype
+c=input("Enter 1st value:")
+d=input("Enter 2nd value:")
+print("sum of both value", c+d)
