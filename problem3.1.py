f634e8cd87ad001809c0637d76aa6a01e121ecd5
aashish50x <aashish50x@gmail.com>
2025-01-31 02:12:35 +0530

    display good afternoon before users Name

---
+++ problem3.1.py
@@ -0,0 +1,6 @@
+# now display good afternoon before users Name
+a = input("Enter your name :")
+print(f"Good Afternoon , {a} ")  #new
+print("Good Afternoon ,", a ," ")   # old technique
+print(f"Good Afternoon ,", a)
+print("Good Afternoon ,", a)
