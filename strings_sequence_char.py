a0101088e0b60a2567cdbc52874c1cbd2fd8d121
aashish50x <aashish50x@gmail.com>
2025-01-31 01:22:31 +0530

    Sequence Characters in string input in Python

---
+++ strings_sequence_char.py
@@ -0,0 +1,22 @@
+# Sequence Characters in Python
+
+# "\n" Newline (Line Break)
+print("Hello everyone ,\nI am Aashish.")
+
+# "/t" Tab Space
+print("Aashish\tKumar")  # "Aashish Kumar"
+
+# "\b"	Backspace (Removes previous character)
+print("Aashishx\b Kumar")	# remove 'x' an print 'Aashish Kumar'
+
+# "\f"	Form Feed (Page Break)
+print("Aashish\fKumar")	  # Aashish␊Kumar
+
+# (\')	Single Quote
+print('Aashish\'s mac')	  # Aashish's mac
+
+# (\")	Double Quote
+print("My self \"Aashish\"")	 # My self "Aashish"
+
+# (\\)	Backslash
+print("C:\\Users\\Aashish")	 # C:\Users\Aashish
