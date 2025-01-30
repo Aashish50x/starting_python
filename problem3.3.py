56077c5fceafa393481c68780614fd646f547270
aashish50x <aashish50x@gmail.com>
2025-01-31 02:14:28 +0530

    finding double space in given input

---
+++ problem3.3.py
@@ -0,0 +1,13 @@
+# finding double space in given input 
+
+line = input("Enter your line :")
+print(line.find("  "))
+
+# finding single space in given input  (problem3.3x)
+
+line = input("Enter your line :")
+print(line.find(" "))
+
+# replace double space by single space in given input  (problem3.4)
+line = input("Enter your line :")
+print(line.replace("  "," "))
