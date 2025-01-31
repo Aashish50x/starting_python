79958e7916fb68eaf3123c1f8a7b04ccdb607de3
aashish50x <aashish50x@gmail.com>
2025-01-31 21:33:02 +0530

    Write a program to store seven fruits in a list entered by the user.

---
+++ problem4.1.py
@@ -0,0 +1,35 @@
+# Write a program to store seven fruits in a list entered by the user.
+
+
+# smart version
+fruits = []
+print("Enter all seven fruits name one by one :")
+f1=input()
+fruits.append(f1)
+f2=input()
+fruits.append(f2)
+f3=input()
+fruits.append(f3)
+f4=input()
+fruits.append(f4)
+f5=input()
+fruits.append(f5)
+f6=input()
+fruits.append(f6)
+f7=input()
+fruits.append(f7)
+print(fruits)
+
+
+# in tuple()
+# basic version
+print("Enter all seven fruits name one by one :")
+a1=input()
+a2=input()
+a3=input()
+a4=input()
+a5=input()
+a6=input()
+a7=input()
+tuple = (a1,a2,a3,a4,a5,a6,a7)
+print(tuple)
