5db4a6ae64d6906f8162843ae8af9a901d7a1ef3
aashish50x <aashish50x@gmail.com>
2025-01-31 16:42:54 +0530

    String and its basic method to print

--- strings.py
+++ strings.py
@@ -1,5 +1,5 @@
 # strings input is taken by simply input 
-
+# strings are inmutal able . the main string will notchange it make another modified string
 a = input("Enter the string: ")
 print("String is : ",a)
 
@@ -8,7 +8,7 @@ print("Printing specific index values: ")
 name = "Aashish Kumar"
 print("String is :", name)
 # printing specific index 
-print(name[1:5])  # from index 1 to 3
+print(name[1:5])  # from index 1 to 4
 print(name[0:5])  # from index 0 to 4
 print(name[0:2])  # from index 0 to 1
 print(name[0:1])  # from index 0 to 1-1 , gives result only index 0, end index will not print
