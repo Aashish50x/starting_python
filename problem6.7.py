bcd79c2696b4ea5083293e172d9d0a7035c361ee
aashish50x <aashish50x@gmail.com>
2025-02-06 16:32:36 +0530

    Write a program to find out whether a given post is talking about “Aashish” or not.

---
+++ problem6.7.py
@@ -0,0 +1,9 @@
+# Write a program to find out whether a given
+# post is talking about “Aashish” or not.
+
+a=input("write your post : ")
+
+if ("Aashish" in a):
+    print("This post is talking about Aashish.")
+else:
+    print("This post is not talking about Aashish.")
