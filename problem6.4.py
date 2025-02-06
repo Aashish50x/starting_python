002e050ad09c1b21409e2b97724613b25c079398
aashish50x <aashish50x@gmail.com>
2025-02-06 16:26:44 +0530

    Write a program to find whether a given username contains less than 10 characters or not.

---
+++ problem6.4.py
@@ -0,0 +1,9 @@
+# Write a program to find whether a given username contains less than 10 
+# characters or not.
+
+a=input("Enter your text : ")
+
+if(len(a)<10):
+    print("it contains less then 10 characters.")
+else:
+    print("it contains more then or equal to 10 characters.")
