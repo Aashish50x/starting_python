677af505f2aaa6141007e733e056bc4f286c190b
aashish50x <aashish50x@gmail.com>
2025-02-06 16:20:09 +0530

    if else & elif example new tecchnique

---
+++ conditional2.py
@@ -0,0 +1,12 @@
+# if else & elif example
+
+a=int(input("Enter your age: "))
+if(a>=18):
+    print("You are above the age of consent")
+if(a==0):
+    print("You are entering zero age.")
+if(a<0):
+    print("You are entering negatve age.")
+if(a<18):
+    print("You are below the age of consent")
+print("End of the program")
